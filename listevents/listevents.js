25cc72911a6fba44a3ab4137aad0535354b1ba68
James <james@jarofgreen.co.uk>
2013-12-15 18:41:09 +0000

    Correct name of widget should be plural

--- listevents/listevents.js
+++ listevents/listevents.js
@@ -3,7 +3,7 @@
  * @license BSD-3 Clause License
  */
 
-var ICanHasACalendarWidgetListEvent = {
+var ICanHasACalendarWidgetListEvents = {
 	cssAdded: false,
 	callBackFunctionCount: 0,
 	place: function(divid, site, options) {
@@ -35,35 +35,35 @@ var ICanHasACalendarWidgetListEvent = {
 
 		var target = usingOptions['openInNewWindow'] ? ' target="_BLANK"' : '';
 
-		div.innerHTML = '<div class="ICanHasACalendarWidgetListEventData">'+
-				'<div class="ICanHasACalendarWidgetListEventHeader"><a href="'+moreURL+'" '+target+' id="'+divid+'Title">'+ICanHasACalendarWidgetListEvent.escapeHTML(usingOptions.title)+'</a></div>'+
-				'<div class="ICanHasACalendarWidgetListEventEvents" id="'+divid+'Data">Loading</div>'+
-				'<div class="ICanHasACalendarWidgetListEventFooter">'+
-					'<div class="ICanHasACalendarWidgetListEventFooterMore"><a href="'+moreURL+'" '+target+'>See more ...</a></div>'+
-					'<div class="ICanHasACalendarWidgetListEventFooterCredit">Powered by <a href="http://ican.hasacalendar.co.uk" '+target+'>Has A Calendar</a></div>'+
+		div.innerHTML = '<div class="ICanHasACalendarWidgetListEventsData">'+
+				'<div class="ICanHasACalendarWidgetListEventsHeader"><a href="'+moreURL+'" '+target+' id="'+divid+'Title">'+ICanHasACalendarWidgetListEvents.escapeHTML(usingOptions.title)+'</a></div>'+
+				'<div class="ICanHasACalendarWidgetListEventsEvents" id="'+divid+'Data">Loading</div>'+
+				'<div class="ICanHasACalendarWidgetListEventsFooter">'+
+					'<div class="ICanHasACalendarWidgetListEventsFooterMore"><a href="'+moreURL+'" '+target+'>See more ...</a></div>'+
+					'<div class="ICanHasACalendarWidgetListEventsFooterCredit">Powered by <a href="http://ican.hasacalendar.co.uk" '+target+'>Has A Calendar</a></div>'+
 				'</div>'+
 			'</div>';
 		var dataDiv = document.getElementById(divid+"Data");
 		var headTag = document.getElementsByTagName('head').item(0);
 
-		if (!ICanHasACalendarWidgetListEvent.cssAdded && usingOptions.loadCSS) {	
+		if (!ICanHasACalendarWidgetListEvents.cssAdded && usingOptions.loadCSS) {	
 			var link = document.createElement("link");
 			link.type = "text/css"; 
 			link.href = "http://hasacalendar.co.uk/widgetcss/listevents.css?v=1"; 
 			link.rel = "stylesheet"; 
 			headTag.appendChild(link);
-			ICanHasACalendarWidgetListEvent.cssAdded = true;
+			ICanHasACalendarWidgetListEvents.cssAdded = true;
 		}
 
-		ICanHasACalendarWidgetListEvent.callBackFunctionCount++;
-		window["ICanHasACalendarWidgetListEventCallBackFunction"+ICanHasACalendarWidgetListEvent.callBackFunctionCount] = function(data) {			
+		ICanHasACalendarWidgetListEvents.callBackFunctionCount++;
+		window["ICanHasACalendarWidgetListEventsCallBackFunction"+ICanHasACalendarWidgetListEvents.callBackFunctionCount] = function(data) {			
 			var html = '';
 			var limit = Math.min(data.data.length, usingOptions.eventCount);
 			if (limit <= 0) {
-				html = '<div class="ICanHasACalendarWidgetListEventEventNone">No events</div>';
+				html = '<div class="ICanHasACalendarWidgetListEventsEventNone">No events</div>';
 			} else {
 				for (var i=0;i<limit;i++) {
-					html += ICanHasACalendarWidgetListEvent.htmlFromEvent(data.data[i], usingOptions.maxStringLength, target);
+					html += ICanHasACalendarWidgetListEvents.htmlFromEvent(data.data[i], usingOptions.maxStringLength, target);
 				}
 			}
 
@@ -87,16 +87,16 @@ var ICanHasACalendarWidgetListEvent = {
 
 		var script = document.createElement("script");
 		script.type = "text/javascript"; 
-		script.src = url+"?callback=ICanHasACalendarWidgetListEventCallBackFunction"+ICanHasACalendarWidgetListEvent.callBackFunctionCount;
+		script.src = url+"?callback=ICanHasACalendarWidgetListEventsCallBackFunction"+ICanHasACalendarWidgetListEvents.callBackFunctionCount;
 		headTag.appendChild(script);
 	},
 	htmlFromEvent: function(event, maxLength, target) {
-		var html = '<div class="ICanHasACalendarWidgetListEventEvent">'
-		html += '<div class="ICanHasACalendarWidgetListEventDate">'+event.start.displaylocal+'</div>';
-		html += '<div class="ICanHasACalendarWidgetListEventSummary"><a href="'+event.siteurl+'" '+target+'>'+ICanHasACalendarWidgetListEvent.escapeHTML(event.summaryDisplay)+'</a></div>';
-		html += '<div class="ICanHasACalendarWidgetListEventDescription">'+ICanHasACalendarWidgetListEvent.escapeHTMLNewLine(event.description, maxLength)+'</div>';
-		html += '<a class="ICanHasACalendarWidgetListEventMoreLink" href="'+event.siteurl+'" '+target+'>More Info</a>';
-		html += '<div class="ICanHasACalendarWidgetListEventClear"></div>';	
+		var html = '<div class="ICanHasACalendarWidgetListEventsEvent">'
+		html += '<div class="ICanHasACalendarWidgetListEventsDate">'+event.start.displaylocal+'</div>';
+		html += '<div class="ICanHasACalendarWidgetListEventsSummary"><a href="'+event.siteurl+'" '+target+'>'+ICanHasACalendarWidgetListEvents.escapeHTML(event.summaryDisplay)+'</a></div>';
+		html += '<div class="ICanHasACalendarWidgetListEventsDescription">'+ICanHasACalendarWidgetListEvents.escapeHTMLNewLine(event.description, maxLength)+'</div>';
+		html += '<a class="ICanHasACalendarWidgetListEventsMoreLink" href="'+event.siteurl+'" '+target+'>More Info</a>';
+		html += '<div class="ICanHasACalendarWidgetListEventsClear"></div>';	
 		return html+'</div>';
 	},			
 	escapeHTML: function(str) {
